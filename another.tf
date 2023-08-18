df14d039b1d2d07c6a6782f5c1b79f1db30cbb4f
daniel2372 <dannyjr1569@gmail.com>
2023-08-18 02:31:31 +0100

    first commit

--- another.tf
+++ another.tf
@@ -72,22 +72,34 @@ data "aws_ami" "amzlinux2" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "dannys3bucket1478"
-
-   versioning {
-        enabled = true
-    }
-      logging {
-        target_bucket = "dannys3bucket1478"
-    }
-   server_side_encryption_configuration {
-     rule {
-       apply_server_side_encryption_by_default {
-         kms_master_key_id = "arn"
-         sse_algorithm     = "aws:kms"
-       }
-     }
-   }
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_logging" "my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  target_bucket = aws_s3_bucket.my_bucket.id
+  target_prefix = "log/"
+}
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 
 resource "aws_s3_bucket_public_access_block" "my_bucket" {
   bucket = aws_s3_bucket.my_bucket.id
